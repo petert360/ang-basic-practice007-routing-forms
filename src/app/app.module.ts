fd49320cce35c5b787d9ecac5e2cef4fece0f726
petert360 <peter.t360@hotmail.com>
2021-02-28 20:46:42 +0100

    Projekt létrehozás, első két feladat megoldása.

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EventsListComponent } from './page/events-list/events-list.component';
+import { NavigationComponent } from './common/navigation/navigation.component';
+import { EventEditorComponent } from './event-editor/event-editor.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EventsListComponent,
+    NavigationComponent,
+    EventEditorComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
